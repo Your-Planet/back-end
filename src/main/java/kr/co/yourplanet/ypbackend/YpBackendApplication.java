b2341bf21581e7b5c91625aa7455ce66eec195d3
이영재 <youngjewall@me.com>
2023-11-01 20:47:34 +0900

    spring boot 최초 커밋

---
+++ src/main/java/kr/co/yourplanet/ypbackend/YpBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.yourplanet.ypbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/yourplanet/ypbackend/YpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.yourplanet.ypbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
