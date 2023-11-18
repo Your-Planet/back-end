5566b472c7d34e1b74e4550a2e7c00c568a1ccfe
이영재 <youngjewall@me.com>
2023-11-18 16:17:54 +0900

    feat : 컨트롤러 전역에서 발생하는 Exception에 대한 Handler 구현

---
+++ src/main/java/kr/co/yourplanet/ypbackend/common/exception/GlobalExceptionHanlder.java
@@ -0,0 +1,25 @@
+package kr.co.yourplanet.ypbackend.common.exception;
+
+import kr.co.yourplanet.ypbackend.common.ResponseForm;
+import kr.co.yourplanet.ypbackend.common.StatusCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHanlder {
+
+    @ExceptionHandler(IllegalStateException.class)
+    protected ResponseEntity<ResponseForm> illegalStateException(IllegalStateException e) {
+        ResponseForm exceptionResponse = new ResponseForm(StatusCode.BAD_REQUEST, e.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ResponseForm> illegalStateException(Exception e) {
+        ResponseForm exceptionResponse = new ResponseForm(StatusCode.BAD_REQUEST, "정의되지 않은 예외 발생 \n" + e.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
