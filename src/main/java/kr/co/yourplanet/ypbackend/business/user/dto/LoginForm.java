0535cf79d075b2c0b51fc38fe8bcbb909cafd4a6
naeunchan <eunchan0492@naver.com>
2023-11-25 15:21:55 +0900

    Add NoArgsConstructor annotaion

--- src/main/java/kr/co/yourplanet/ypbackend/business/user/dto/LoginForm.java
+++ src/main/java/kr/co/yourplanet/ypbackend/business/user/dto/LoginForm.java
@@ -1,9 +1,11 @@
 package kr.co.yourplanet.ypbackend.business.user.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
 public class LoginForm {
 
     private String id;
