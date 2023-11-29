41ec8aec693a8afc5fa7ce5d2c4f150e17fcd169
DuFong <youngjewall@me.com>
2023-11-30 01:15:42 +0900

    ResponseForm generic type으로 변경

--- src/main/java/kr/co/yourplanet/ypbackend/common/ResponseForm.java
+++ src/main/java/kr/co/yourplanet/ypbackend/common/ResponseForm.java
@@ -1,29 +1,20 @@
 package kr.co.yourplanet.ypbackend.common;
 
 import kr.co.yourplanet.ypbackend.common.enums.StatusCode;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-public class ResponseForm {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseForm<T> {
 
     private StatusCode statusCode;
     private String message;
-    private Object data;
-
-    public ResponseForm()
-    {
-        statusCode = null;
-        message = null;
-        data = null;
-    }
-
-    public ResponseForm(StatusCode statusEnum, String message, Object data) {
-        this.statusCode = statusEnum;
-        this.message = message;
-        this.data = data;
-    }
+    private T data;
 
     public ResponseForm(StatusCode statusEnum) {
         this.statusCode = statusEnum;
@@ -33,11 +24,11 @@ public class ResponseForm {
 
     public ResponseForm(StatusCode statusEnum, String message) {
         this.statusCode = statusEnum;
-        this.data = null;
         this.message = message;
+        this.data = null;
     }
 
-    public ResponseForm(StatusCode statusEnum, Object data) {
+    public ResponseForm(StatusCode statusEnum, T data) {
         this.statusCode = statusEnum;
         this.message = null;
         this.data = data;
--- src/main/java/kr/co/yourplanet/ypbackend/common/exception/GlobalExceptionHanlder.java
+++ src/main/java/kr/co/yourplanet/ypbackend/common/exception/GlobalExceptionHanlder.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHanlder {
 
     @ExceptionHandler(IllegalStateException.class)
-    protected ResponseEntity<ResponseForm> illegalStateException(IllegalStateException e) {
-        ResponseForm exceptionResponse = new ResponseForm(StatusCode.BAD_REQUEST, e.getMessage());
+    protected ResponseEntity<ResponseForm<?>> illegalStateException(IllegalStateException e) {
+        ResponseForm<?> exceptionResponse = new ResponseForm<>(StatusCode.BAD_REQUEST, e.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ResponseForm> illegalStateException(Exception e) {
-        ResponseForm exceptionResponse = new ResponseForm(StatusCode.BAD_REQUEST, "정의되지 않은 예외 발생 \n" + e.getMessage());
+    protected ResponseEntity<ResponseForm<?>> etcException(Exception e) {
+        ResponseForm<?> exceptionResponse = new ResponseForm<>(StatusCode.BAD_REQUEST, "정의되지 않은 예외 발생 \n" + e.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
