21886b03d07712038d1ebe90449eca6084327eae
이영재 <youngjewall@me.com>
2024-02-24 15:48:18 +0900

    feat : Task.title 추가 및 Task.categoryList 수정

--- src/main/java/kr/co/yourplanet/ypbackend/common/exception/GlobalExceptionHanlder.java
+++ src/main/java/kr/co/yourplanet/ypbackend/common/exception/GlobalExceptionHanlder.java
@@ -2,12 +2,14 @@ package kr.co.yourplanet.ypbackend.common.exception;
 
 import kr.co.yourplanet.ypbackend.common.ResponseForm;
 import kr.co.yourplanet.ypbackend.common.enums.StatusCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHanlder {
 
     @ExceptionHandler(IllegalStateException.class)
@@ -24,6 +26,7 @@ public class GlobalExceptionHanlder {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ResponseForm<?>> etcException(Exception e) {
+        log.error("오류 발생", e);
         ResponseForm<?> exceptionResponse = new ResponseForm<>(StatusCode.BAD_REQUEST, "정의되지 않은 예외 발생 : " + e.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
