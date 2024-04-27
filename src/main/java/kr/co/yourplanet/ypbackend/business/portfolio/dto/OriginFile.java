a69e754cc858c8ea8f45b8e15f5867cbd0650cf3
hyunwoo_kim3 <hyunwoo_kim3@innodep.com>
2024-04-28 05:05:07 +0900

    Feat : 임시/최종 가격 저장 및 조회 API 추가

---
+++ src/main/java/kr/co/yourplanet/ypbackend/business/portfolio/dto/OriginFile.java
@@ -0,0 +1,18 @@
+package kr.co.yourplanet.ypbackend.business.portfolio.dto;
+
+import kr.co.yourplanet.ypbackend.common.enums.PriceOptionType;
+import kr.co.yourplanet.ypbackend.common.interfaces.ValidEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OriginFile {
+    @ValidEnum(enumClass = PriceOptionType.class)
+    public PriceOptionType provisionType;
+    public int price;
+}
