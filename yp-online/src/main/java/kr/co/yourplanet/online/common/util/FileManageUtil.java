6d7e0387f695b67719e80652a429432f97a9f09e
이영재 <youngjewall@me.com>
2024-07-08 23:50:15 +0900

    [feat] Studio 프로필 이미지 validation 추가 Jira: YP-379

--- yp-online/src/main/java/kr/co/yourplanet/online/common/util/FileManageUtil.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/common/util/FileManageUtil.java
@@ -7,18 +7,24 @@ import kr.co.yourplanet.online.properties.FileProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class FileManageUtil {
+
+    private static final List<String> IMAGE_VALID_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png", "gif");
+    private static long MAX_IMAGE_SIZE = 5 * 1024 * 1024;
 
     private final FileProperties fileProperties;
 
@@ -53,10 +59,36 @@ public class FileManageUtil {
         }
     }
 
+    public void validateImageFile(MultipartFile file) {
+
+        validateFile(file);
+
+        String fileName = file.getOriginalFilename();
+        if (!StringUtils.hasText(fileName)) {
+            throw new BusinessException(StatusCode.BAD_REQUEST, "파일명이 존재하지 않습니다.", false);
+        }
+
+        // 이미지 파일 확장자 검사
+        if (!IMAGE_VALID_EXTENSIONS.contains(getFileExtension(fileName))) {
+            throw new BusinessException(StatusCode.BAD_REQUEST, "유효하지 않은 파일 확장자입니다.", false);
+        }
+
+        // 이미지 파일 최대 용량 확인
+        if (MAX_IMAGE_SIZE < file.getSize()) {
+            throw new BusinessException(StatusCode.BAD_REQUEST, "이미지는 최대 5MB까지 업로드 가능합니다.", false);
+        }
+
+    }
+
     private void validateFile(MultipartFile file) {
         if (file == null || file.isEmpty()) {
             throw new BusinessException(StatusCode.BAD_REQUEST, "빈 파일은 저장할 수 없습니다.", false);
         }
+    }
+
+    private String getFileExtension(String fileName) {
+        String[] parts = fileName.split("\\.");
+        return parts.length > 1 ? parts[parts.length - 1] : "";
     }
 
     private Path generateFileAbsolutePath(FileType fileType, String fileName) {
