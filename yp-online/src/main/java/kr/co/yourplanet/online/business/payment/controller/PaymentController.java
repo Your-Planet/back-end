5e2b2ca9b24a2351606a6267cc511bfe2dcc71e5
Lee ChanMi <anytime0224@gmail.com>
2025-01-15 09:15:37 +0900

    [YP-530] feat: 결제 승인 API 구현

--- yp-online/src/main/java/kr/co/yourplanet/online/business/payment/controller/PaymentController.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/business/payment/controller/PaymentController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import kr.co.yourplanet.core.enums.StatusCode;
-import kr.co.yourplanet.online.business.payment.dto.request.PaymentRequestForm;
+import kr.co.yourplanet.online.business.payment.controller.dto.request.PaymentApproveForm;
+import kr.co.yourplanet.online.business.payment.controller.dto.request.PaymentRequestForm;
 import kr.co.yourplanet.online.business.payment.service.PaymentRequestService;
+import kr.co.yourplanet.online.business.payment.service.PaymentService;
 import kr.co.yourplanet.online.common.ResponseForm;
 import kr.co.yourplanet.online.jwt.JwtPrincipal;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +24,24 @@ import lombok.RequiredArgsConstructor;
 public class PaymentController {
 
     private final PaymentRequestService paymentRequestService;
+    private final PaymentService paymentService;
 
     @PostMapping("/request")
     public ResponseEntity<ResponseForm<Void>> savePaymentRequest(
             @AuthenticationPrincipal JwtPrincipal principal,
-            @RequestBody PaymentRequestForm request
+            @Valid @RequestBody PaymentRequestForm request
     ) {
-        paymentRequestService.savePaymentRequest(principal.getId(), request.orderId(), request.amount());
+        paymentRequestService.save(principal.getId(), request.orderId(), request.amount());
+
+        return new ResponseEntity<>(new ResponseForm<>(StatusCode.OK), HttpStatus.OK);
+    }
+
+    @PostMapping("/approve")
+    public ResponseEntity<ResponseForm<Void>> approvePayment(
+            @AuthenticationPrincipal JwtPrincipal principal,
+            @Valid @RequestBody PaymentApproveForm request
+    ) {
+        paymentService.approve(principal.getId(), request.paymentKey(), request.orderId(), request.amount());
 
         return new ResponseEntity<>(new ResponseForm<>(StatusCode.OK), HttpStatus.OK);
     }
---
+++ yp-online/src/main/java/kr/co/yourplanet/online/business/payment/controller/dto/request/PaymentApproveForm.java
@@ -0,0 +1,18 @@
+package kr.co.yourplanet.online.business.payment.controller.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record PaymentApproveForm(
+        @NotEmpty(message = "paymentKey는 빈 문자열일 수 없습니다.")
+        String paymentKey,
+
+        @NotEmpty(message = "orderId는 빈 문자열일 수 없습니다.")
+        String orderId,
+
+        @NotNull(message = "amount는 null일 수 없습니다.")
+        @Min(value = 0L, message = "amount는 0 이하일 수 없습니다.")
+        Long amount
+) {
+}
