a8e3f7c929e97e6c94ab3c2b7ba3f6da2f1e06e7
이영재 <youngjewall@me.com>
2025-02-04 22:21:51 +0900

    Revert "[YP-664] refactor : Jackson 날짜 형식 지정 Config 추가"
    This reverts commit ae95af0e1d35caedb12c8cfb1ef6bf399087061e.

--- yp-online/src/main/java/kr/co/yourplanet/online/business/project/dto/response/ProjectDetailInfo.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/business/project/dto/response/ProjectDetailInfo.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import kr.co.yourplanet.core.enums.ProjectStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,11 +23,16 @@ public class ProjectDetailInfo {
     private List<ProjectHistoryForm> projectHistories;
 
     private ProjectStatus projectStatus;
-    
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime requestDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime negotiateDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime acceptDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime completeDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime rejectDateTime;
 
     @Getter
@@ -36,6 +43,7 @@ public class ProjectDetailInfo {
         private String sponsorName;
         private String creatorName;
         private String brandName;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
         private LocalDate dueDate;
         private Integer defaultPanelCount;
         private Integer additionalPanelCount;
--- yp-online/src/main/java/kr/co/yourplanet/online/business/project/dto/response/ProjectHistoryForm.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/business/project/dto/response/ProjectHistoryForm.java
@@ -3,6 +3,8 @@ package kr.co.yourplanet.online.business.project.dto.response;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import kr.co.yourplanet.core.entity.project.ProjectHistory;
 import kr.co.yourplanet.core.enums.MemberType;
 import kr.co.yourplanet.online.business.project.dto.request.ProjectCommonAttribute;
@@ -44,11 +46,13 @@ public class ProjectHistoryForm {
      */
     private ProjectCommonAttribute.ProjectPostDurationExtension postDurationExtension;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private List<LocalDate> postStartDates;
 
     /**
      * 작업 기한
      */
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate dueDate;
 
     /**
