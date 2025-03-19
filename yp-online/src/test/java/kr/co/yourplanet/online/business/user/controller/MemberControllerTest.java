46689656f6d58ef90568105c641b80aa4610fcb8
Lee ChanMi <anytime0224@gmail.com>
2025-03-19 17:33:41 +0900

    [YP-750] test: 내 정보 조회 API 테스트 작성

--- yp-online/src/test/java/kr/co/yourplanet/online/business/user/controller/MemberControllerTest.java
+++ yp-online/src/test/java/kr/co/yourplanet/online/business/user/controller/MemberControllerTest.java
@@ -19,7 +19,75 @@ import kr.co.yourplanet.template.IntegrationTest;
 class MemberControllerTest extends IntegrationTest {
 
     @Nested
-    @DisplayName("멤버 정보 수정 API 테스트")
+    @DisplayName("내 정보 조회 API")
+    class GetMyMemberInfo {
+
+        private final String path = "/members/me";
+
+        @DisplayName("[성공] 작가 사업자 - 내 정보 조회에 성공한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 1L, memberType = MemberType.CREATOR)
+        void success_creator_business() throws Exception {
+            mockMvc.perform(get(path)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.birthDate").isNotEmpty())
+                    .andExpect(jsonPath("$.data.genderType").isNotEmpty())
+                    .andExpect(jsonPath("$.data.companyName").isNotEmpty())
+                    .andExpect(jsonPath("$.data.businessNumber").isNotEmpty())
+                    .andExpect(jsonPath("$.data.representativeName").isNotEmpty())
+                    .andExpect(jsonPath("$.data.businessAddress").isNotEmpty());
+        }
+
+        @DisplayName("[성공] 작가 개인 - 내 정보 조회에 성공한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 2L, memberType = MemberType.CREATOR)
+        void success_creator_individual() throws Exception {
+            mockMvc.perform(get(path)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.birthDate").isNotEmpty())
+                    .andExpect(jsonPath("$.data.genderType").isNotEmpty());
+        }
+
+        @DisplayName("[성공] 광고주 사업자 - 내 정보 조회에 성공한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 3L, memberType = MemberType.SPONSOR)
+        void success_sponsor_business() throws Exception {
+            mockMvc.perform(get(path)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.name").isNotEmpty())
+                    .andExpect(jsonPath("$.data.tel").isNotEmpty())
+                    .andExpect(jsonPath("$.data.companyName").isNotEmpty())
+                    .andExpect(jsonPath("$.data.businessNumber").isNotEmpty())
+                    .andExpect(jsonPath("$.data.representativeName").isNotEmpty())
+                    .andExpect(jsonPath("$.data.businessAddress").isNotEmpty());
+        }
+
+        @DisplayName("[성공] 광고주 개인 - 내 정보 조회에 성공한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 4L, memberType = MemberType.SPONSOR)
+        void success_sponsor_individual() throws Exception {
+            mockMvc.perform(get(path)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.name").isNotEmpty())
+                    .andExpect(jsonPath("$.data.tel").isNotEmpty())
+                    .andExpect(jsonPath("$.data.birthDate").isNotEmpty());
+        }
+    }
+
+    @Nested
+    @DisplayName("멤버 정보 수정 API")
     class UpdateMemberInfo {
 
         private final String path = "/members/me";
@@ -27,7 +95,7 @@ class MemberControllerTest extends IntegrationTest {
         @DisplayName("[성공] 작가 사업자 - 멤버 정보 수정에 성공한다.")
         @Test
         @WithMockJwtPrincipal(id = 1L, memberType = MemberType.CREATOR)
-        void success_creator_individual() throws Exception {
+        void success_creator_business() throws Exception {
             mockMvc.perform(patch(path)
                             .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
                             .contentType(MediaType.APPLICATION_JSON)
@@ -39,7 +107,7 @@ class MemberControllerTest extends IntegrationTest {
         @DisplayName("[성공] 작가 개인 - 멤버 정보 수정에 성공한다.")
         @Test
         @WithMockJwtPrincipal(id = 2L, memberType = MemberType.CREATOR)
-        void success_creator_business() throws Exception {
+        void success_creator_individual() throws Exception {
             mockMvc.perform(patch(path)
                             .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
                             .contentType(MediaType.APPLICATION_JSON)
