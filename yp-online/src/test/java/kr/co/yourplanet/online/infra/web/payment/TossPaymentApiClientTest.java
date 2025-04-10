ceeeea330117ede27b21ca2c22e6b07ed7c5af38
Lee ChanMi <anytime0224@gmail.com>
2025-04-10 16:07:25 +0900

    [YP-750] fix: mock web server 초기화 에러 해결

--- yp-online/src/test/java/kr/co/yourplanet/online/infra/web/payment/TossPaymentApiClientTest.java
+++ yp-online/src/test/java/kr/co/yourplanet/online/infra/web/payment/TossPaymentApiClientTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
