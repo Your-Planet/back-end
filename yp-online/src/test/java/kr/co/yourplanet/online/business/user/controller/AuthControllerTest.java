52b1f48d91973f620d0cdfd2c6231089124dea86
Lee ChanMi <anytime0224@gmail.com>
2025-04-24 23:10:38 +0900

    [YP-811] test: 비밀번호 변경 API 테스트 작성

--- yp-online/src/test/java/kr/co/yourplanet/online/business/user/controller/AuthControllerTest.java
+++ yp-online/src/test/java/kr/co/yourplanet/online/business/user/controller/AuthControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 
 import kr.co.yourplanet.core.enums.MemberType;
 import kr.co.yourplanet.helper.WithMockJwtPrincipal;
+import kr.co.yourplanet.online.business.user.dto.request.ChangePasswordForm;
 import kr.co.yourplanet.online.business.user.dto.request.ValidatePasswordForm;
 import kr.co.yourplanet.online.common.HeaderConstant;
 import kr.co.yourplanet.stub.MemberStub;
@@ -51,6 +52,55 @@ class AuthControllerTest extends IntegrationTest {
                             .content(toJson(form)))
                     .andDo(print())
                     .andExpect(status().isUnauthorized());
+        }
+    }
+
+    @Nested
+    @DisplayName("비밀번호 변경 API")
+    class changePassword {
+
+        private final String path = "/auth/change-password";
+
+        @Test
+        @DisplayName("[성공] 비밀번호 변경에 성공한다.")
+        @WithMockJwtPrincipal(id = 1L, memberType = MemberType.CREATOR)
+        void success() throws Exception {
+            ChangePasswordForm form = new ChangePasswordForm("hello123@");
+
+            mockMvc.perform(patch(path)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(toJson(form)))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("[실패] 직전의 비밀번호를 입력하면 비밀번호 변경에 실패한다.")
+        @WithMockJwtPrincipal(id = 1L, memberType = MemberType.CREATOR)
+        void fail_when_previous_reused_password() throws Exception {
+            ChangePasswordForm form = new ChangePasswordForm(MemberStub.getPassword());
+
+            mockMvc.perform(patch(path)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(toJson(form)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("[실패] 비밀번호 형식이 유효하지 않으면 비밀번호 변경에 실패한다.")
+        @WithMockJwtPrincipal(id = 1L, memberType = MemberType.CREATOR)
+        void fail_when_password_format_invalid() throws Exception {
+            ChangePasswordForm form = new ChangePasswordForm("hello");
+
+            mockMvc.perform(patch(path)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(toJson(form)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
         }
     }
 }
