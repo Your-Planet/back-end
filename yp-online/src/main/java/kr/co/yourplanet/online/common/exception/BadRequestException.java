3107a9c6fdef867396391d2f272b3edaf426e37e
Lee ChanMi <anytime0224@gmail.com>
2025-04-29 17:06:34 +0900

    [YP-735] feat: 서버 내부 에러를 위한 비즈니스 에러 구현

--- yp-online/src/main/java/kr/co/yourplanet/online/common/exception/BadRequestException.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/common/exception/BadRequestException.java
@@ -5,7 +5,6 @@ import kr.co.yourplanet.core.enums.StatusCode;
 public class BadRequestException extends BusinessException {
 
     public BadRequestException(String message) {
-
         super(StatusCode.BAD_REQUEST, message, true);
     }
 }
