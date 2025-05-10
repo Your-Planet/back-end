acb69c546905783a2213a0b1d8a331b616768165
Lee ChanMi <anytime0224@gmail.com>
2025-05-10 21:50:32 +0900

    [YP-735] feat: 결제상태/정산상태 별 작가 프로젝트 정산 건수 조회 API 구현

--- yp-online/src/main/java/kr/co/yourplanet/online/business/settlement/service/ProjectSettlementQueryService.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/business/settlement/service/ProjectSettlementQueryService.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.yourplanet.core.entity.settlement.ProjectSettlement;
+import kr.co.yourplanet.core.entity.settlement.SettlementPaymentStatus;
+import kr.co.yourplanet.core.entity.settlement.SettlementStatus;
 import kr.co.yourplanet.core.enums.StatusCode;
 import kr.co.yourplanet.online.business.settlement.repository.ProjectSettlementRepository;
+import kr.co.yourplanet.online.business.user.service.MemberValidationService;
 import kr.co.yourplanet.online.common.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 
@@ -14,10 +17,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProjectSettlementQueryService {
 
+    private final MemberValidationService memberValidationService;
+
     private final ProjectSettlementRepository projectSettlementRepository;
 
     public ProjectSettlement getByProjectId(long projectId) {
         return projectSettlementRepository.findByProjectId(projectId)
                 .orElseThrow(() -> new BusinessException(StatusCode.NOT_FOUND, "요청한 정산 정보를 찾을 수 없습니다.", true));
+    }
+
+    public long countByStatus(long requesterId, SettlementPaymentStatus paymentStatus, SettlementStatus settlementStatus) {
+        memberValidationService.validateIsCreator(requesterId);
+
+        return projectSettlementRepository.countByStatus(requesterId, paymentStatus, settlementStatus);
     }
 }
