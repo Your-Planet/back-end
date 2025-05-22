3846cdde0f0b23c8b27ca84df2883720e08ee4c9
Lee ChanMi <anytime0224@gmail.com>
2025-05-22 15:41:59 +0900

    [YP-830] feat: redis string 저장소와 object 저장소로 문자열 값 / 해시 값 저장 메서드 추가

--- yp-online/src/main/java/kr/co/yourplanet/online/infra/redis/RedisRepository.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/infra/redis/RedisRepository.java
@@ -1,9 +1,11 @@
 package kr.co.yourplanet.online.infra.redis;
 
 import java.time.Duration;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,22 +14,57 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RedisRepository {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
+    private final RedisTemplate<String, Object> objectRedisTemplate;
 
-    public void saveHashWithTTL(String key, String hashKey, Object value, long ttlInSeconds) {
-        redisTemplate.opsForHash().put(key, hashKey, value);
-        redisTemplate.expire(key, Duration.ofSeconds(ttlInSeconds));
+    /**
+     * 저장
+     */
+    public void save(String key, String value, long ttlInSeconds) {
+        stringRedisTemplate.opsForValue().set(key, value, Duration.ofSeconds(ttlInSeconds));
     }
 
+    public void saveHash(String key, String hashKey, Object value, long ttlInSeconds) {
+        objectRedisTemplate.opsForHash().put(key, hashKey, value);
+        objectRedisTemplate.expire(key, Duration.ofSeconds(ttlInSeconds));
+    }
+
+    public void saveAllHash(String key, Map<String, Object> data, long ttlInSeconds) {
+        objectRedisTemplate.boundHashOps(key).putAll(data);
+        objectRedisTemplate.expire(key, Duration.ofSeconds(ttlInSeconds));
+    }
+
+    /**
+     * 조회
+     */
+    public Optional<String> getValue(String key) {
+        return Optional.ofNullable(stringRedisTemplate.opsForValue().get(key));
+    }
+
+    public Optional<Object> getHashValue(String key, String hashKey) {
+        return Optional.ofNullable(objectRedisTemplate.opsForHash().get(key, hashKey));
+    }
+
+    public Optional<Map<Object, Object>> getAllHashValue(String key) {
+        return Optional.ofNullable(objectRedisTemplate.boundHashOps(key).entries());
+    }
+
+    /**
+     * 검증
+     */
     public boolean hasKey(String key) {
         if (key == null) {
             return false;
         }
 
-        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+        return Boolean.TRUE.equals(stringRedisTemplate.hasKey(key));
     }
 
-    public Optional<Object> getHashValue(String key, String hashKey) {
-        return Optional.ofNullable(redisTemplate.opsForHash().get(key, hashKey));
+    public boolean hasHashKey(String key) {
+        if (key == null) {
+            return false;
+        }
+
+        return Boolean.TRUE.equals(objectRedisTemplate.hasKey(key));
     }
 }
