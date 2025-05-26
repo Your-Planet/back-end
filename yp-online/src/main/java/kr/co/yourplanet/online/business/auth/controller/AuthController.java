b9f404a298f195fea0d4d5dc300beddfc1b2afd1
Lee ChanMi <anytime0224@gmail.com>
2025-05-26 16:21:13 +0900

    [YP-830] chore: auth controller 패키지 이전

--- yp-online/src/main/java/kr/co/yourplanet/online/business/auth/controller/AuthController.java
+++ yp-online/src/main/java/kr/co/yourplanet/online/business/auth/controller/AuthController.java
@@ -1,4 +1,4 @@
-package kr.co.yourplanet.online.business.user.controller;
+package kr.co.yourplanet.online.business.auth.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
