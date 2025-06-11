fc51fa47ce0a98726cc247e1a1334294fdbfa431
Lee ChanMi <anytime0224@gmail.com>
2025-06-11 16:00:51 +0900

    [YP-735] test: 프로젝트 정산 목록 조회, 정산 상세 조회 API 테스트 작성

--- yp-online/src/test/java/kr/co/yourplanet/online/business/settlement/controller/SettlementControllerTest.java
+++ yp-online/src/test/java/kr/co/yourplanet/online/business/settlement/controller/SettlementControllerTest.java
@@ -50,4 +50,66 @@ class SettlementControllerTest extends IntegrationTest {
                     .andExpect(status().isOk());
         }
     }
+
+    @Nested
+    @DisplayName("프로젝트 정산 목록 조회 API")
+    class GetProjectSettlementSummaries {
+
+        private static final String PATH = "/settlement/project";
+
+        @DisplayName("[성공] 관리자는 프로젝트 정산 목록 조회에 성공한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 0L, memberType = MemberType.ADMIN)
+        void success_admin() throws Exception {
+            mockMvc.perform(get(PATH)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .param("page", "0")
+                            .param("size", "10"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+
+        @DisplayName("[실패] 관리자가 아니라면 프로젝트 정산 목록 조회에 실패한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 1L, memberType = MemberType.CREATOR)
+        void fail_when_is_not_admin() throws Exception {
+            mockMvc.perform(get(PATH)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .param("page", "0")
+                            .param("size", "10"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+    }
+
+    @Nested
+    @DisplayName("프로젝트 정산 상세 조회 API")
+    class GetProjectSettlementDetail {
+
+        private static final String PATH = "/settlement/project/{projectId}";
+
+        @DisplayName("[성공] 관리자는 프로젝트 정산 상세 조회에 성공한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 0L, memberType = MemberType.ADMIN)
+        void success_admin() throws Exception {
+            mockMvc.perform(get(PATH, 1)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+
+        @DisplayName("[실패] 관리자가 아니라면 프로젝트 정산 목록 조회에 실패한다.")
+        @Test
+        @WithMockJwtPrincipal(id = 1L, memberType = MemberType.CREATOR)
+        void fail_when_is_not_admin() throws Exception {
+            mockMvc.perform(get(PATH, 1)
+                            .header(HeaderConstant.ACCESS_TOKEN, TokenStub.getMockAccessToken())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+    }
 }
